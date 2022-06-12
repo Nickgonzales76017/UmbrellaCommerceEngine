0eaafdc21311c7815e67d0522fb70896f4f2b872
Nickgonzales76017 <45552585+Nickgonzales76017@users.noreply.github.com>
2022-06-11 20:45:21 -0500

    test2
    test2

--- medusa-config.js
+++ medusa-config.js
@@ -32,14 +32,17 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-     database_url: DATABASE_URL,
-     database_type: "postgres",
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
     //database_database: "./medusa-db.sql",
     //database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
   },
   plugins,
 };
